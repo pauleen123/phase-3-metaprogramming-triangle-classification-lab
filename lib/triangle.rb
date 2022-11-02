f7cb01c12e8959d957edf630235d092bf5225641
+ <+>
2022-11-02 10:17:53 +0300

    commit tree 26174512d3b25c2e9b9345c8ba5009393d67a573

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,43 @@
-class Triangle
-  # write code here
-end
+  class Triangle
+    # write code here
+    attr_accessor :length1, :length2, :length3
+  
+    def initialize(length1, length2, length3)
+      @length1 = length1
+      @length2 = length2
+      @length3 = length3
+    end
+  
+    def kind
+      if triangle_valid? && triangle_existent?
+        if @length1 == @length2 && @length1 == @length3
+          :equilateral
+        elsif @length1 == @length2 || @length1 == @length3 || @length2 == @length3
+          :isosceles
+        else
+          :scalene
+        end
+      else
+        raise TriangleError
+      end
+    end
+  
+    private
+  
+    def triangle_valid?
+      @length1 + @length2 > @length3 &&
+      @length2 + @length3 > @length1 &&
+      @length1 + @length3 > @length2
+    end
+  
+    def triangle_existent?
+      [length1, length2, length3].all? { |length| length > 0 }
+    end
+  
+    class TriangleError < StandardError
+      def msg
+        "GUYS! Come look at this clown who doesn't know what Triangles look like!"
+      end
+    end
+  
+  end
